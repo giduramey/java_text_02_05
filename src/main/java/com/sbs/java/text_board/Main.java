6e1f26882eaeaea426f1a48f72a4fdd0ef3354c7
giduramey <gosock543211@gmail.com>
2025-02-05 20:21:31 +0900

    urs/article/list 명령어 처리, 게시물 리스트 출력

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -1,5 +1,7 @@
 package com.sbs.java.text_board;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,7 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     int lastArticleId = 0;
     Article lastArticle = null;
+    List<Article> articles = new ArrayList<>();
 
     System.out.println("== 자바 텍스트 게시판 시작==");
 
@@ -14,7 +17,7 @@ public class Main {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("/usr/article/write")) {
+      if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
         System.out.print("제목 : ");
         String subject = sc.nextLine();
@@ -27,35 +30,53 @@ public class Main {
         Article article = new Article(id, subject, content);
         lastArticle = article;
 
-        System.out.println("생성된 게시물 객체 : " + article);
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+        articles.add(article);
 
-      }
-      else if(cmd.equals("/usr/article/detail")) {
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+      } else if (cmd.equals("/usr/article/list")) {
+        if (articles.isEmpty()) {
+          System.out.println("현재 게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("==게시물 리스트==");
+        System.out.println("번호 | 제목");
+
+//        for (int i = 0; i < articles.size(); i++) {
+//          Article article = articles.get(i);
+//          System.out.printf("%d | %s\n", article.id, article.subject);
+//        }
+
+        articles.forEach(
+            article -> System.out.printf("%d | %s\n", article.id, article.subject)
+        );
+
+      } else if (cmd.equals("/usr/article/detail")) {
         Article article = lastArticle;
-        if(article == null) {
+        if (article == null) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
+
 
         System.out.println("== 게시물 상세보기 ==");
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.subject);
         System.out.printf("내용 : %s\n", article.content);
-      }
-      else if(cmd.equals("exit")){
+      } else if (cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다");
         break;
-      }
-      else {
+      } else {
         System.out.println("잘못된 명령어입니다.");
       }
     }
 
-    System.out.println("== 자바 텍스트 게시판 종료==");
-    sc.close();
+
+      System.out.println("== 자바 텍스트 게시판 종료==");
+      sc.close();
+    }
   }
-}
+
 
 class Article {
   int id;
