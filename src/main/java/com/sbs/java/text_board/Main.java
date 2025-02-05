f75b8c0496ad0df44b5a0ef47fc90f2d7b873787
giduramey <gosock543211@gmail.com>
2025-02-05 15:41:10 +0900

    게시물 작성시 작성된 정보를 Article 객체의 저장

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -20,9 +20,15 @@ public class Main {
         System.out.print("내용 : ");
         String content = sc.nextLine();
 
-        int id = 1;
+        int id = ++lastArticleId;
 
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+        Article article = new Article();
+        article.id = id;
+        article.subject = subject;
+        article.content = content;
+
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
 
       }
       else if(cmd.equals("exit")){
@@ -37,4 +43,10 @@ public class Main {
     System.out.println("== 자바 텍스트 게시판 종료==");
     sc.close();
   }
+}
+
+class Article {
+  int id;
+  String subject;
+  String content;
 }
