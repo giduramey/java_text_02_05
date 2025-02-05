c1e99ca9c44d01ad2a8ac4250cebdc82431d3b97
giduramey <gosock543211@gmail.com>
2025-02-05 10:54:14 +0900

    스캐너를 이용한 명령어 입력받기

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -1,8 +1,16 @@
 package com.sbs.java.text_board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
     System.out.println("== 자바 텍스트 게시판 시작==");
+    System.out.print("명령) ");
+    String cmd = sc.nextLine();
+
+    System.out.printf("입력받은 명령어 : %s\n", cmd);
     System.out.println("== 자바 텍스트 게시판 종료==");
   }
 }
