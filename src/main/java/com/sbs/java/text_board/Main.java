408fe3aa36f1070d24b826673519591d647271ef
giduramey <gosock543211@gmail.com>
2025-02-05 10:47:57 +0900

    메세지 출력

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -2,6 +2,7 @@ package com.sbs.java.text_board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("== 자바 텍스트 게시판 시작==");
+    System.out.println("== 자바 텍스트 게시판 종료==");
   }
 }
